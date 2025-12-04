2026f642324f4d0bd2fb964f8dba289c8217158b
xuanday <vanxuaniq2@gmail.com>
2025-12-04 16:46:07 +0700

    thay doi moi

--- app/Backend/server.js
+++ app/Backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3069;
 
 // --- setup swagger ---
 setupSwagger(app);
